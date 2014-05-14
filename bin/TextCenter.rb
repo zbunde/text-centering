1ba1e6affd6e1a096290e3417a2a02bcaf5e5665
Zach Klabunde <zbunde@gmail.com>
2014-05-14 09:57:01 -0600

    hmmmmmmmm

--- bin/TextCenter.rb
+++ bin/TextCenter.rb
@@ -1,18 +1,36 @@
 class TextCenter
 
   def initialize(text)
-    @text = text
+    @text = text.split("\n")
+
   end
 
-  def lines
-    @text.split("/n")
-  end
 
+
+# center the text based on longest string
+  
   def center
-    lines.each do |x|
-     x.center(20)
+    length = longest_string(@text)
+
+
+    @text.map do |line|
+      if line.length < length
+
+
+      end
     end
-    @text.to_s
+
+
+  end
+
+
+#find the longest string in the text
+
+  def longest_string(text)
+    length = text.map do |line|
+      line.length
+    end
+      length.sort.last
   end
 
 end
